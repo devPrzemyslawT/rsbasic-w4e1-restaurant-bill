23e2b158200922199bc9487f4364d038a4d72069
devPrzemyslawT <przemyslaw.tomaszewski.dev@gmail.com>
2023-02-27 06:36:56 +0100

    project simple form structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<div className='App-title'>
+				<h1> Bill calculator </h1>
+				<h2> Calculate yours bill!</h2>
+			</div>
+			<div className='App-body'>
+				<form>
+					<p>Value</p>
+					<input type='number'></input>
+					<p>Tip</p>
+					<input type='number'></input>
+
+					<p>Tax</p>
+					<input type='number'></input>
+					<div className='App-submit'>
+						<button>Submit</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
 }
 
 export default App;
